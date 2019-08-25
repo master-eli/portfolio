4064a97141f5dc0e48182201367bcc51e36530eb
Elizear John Acosta Galapia <32735402+elii11@users.noreply.github.com>
2019-08-26 00:16:30 +0800

    svg bg fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="home">
-      <div class="name">
+      <div class="name" :style="{ backgroundImage: `url(${image})` }">
             <div class="text-holder">
                 <h1>elizear john galapia.</h1>
                 <h1 class="animated-text"> web developer <span>></span></h1>
             </div>
-      </div>
-      <div class="svg-bg">
-          <img src="../assets/bg-intro.svg" alt="">
       </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            image: require('../assets/bg-intro.svg'),
+            publicPath: process.env.BASE_URL,
+            styleObject: {
+                backgroundImage: `url($(this.image))`
+            }
+        }
+    }
 }
 </script>
 
